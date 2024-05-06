3298d41a6fba4b3f396c84de6a0c60645c5f46c2
AbrahamKan <132621122+AbrahamKan@users.noreply.github.com>
2024-05-05 22:48:06 -0600

    Cambios nuevos

--- carniceria/src/main/java/com/app/controllers/App.java
+++ carniceria/src/main/java/com/app/controllers/App.java
@@ -28,6 +28,7 @@ public class App extends Application {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/primary.fxml"));
         Parent root = loader.load();
         scene = new Scene(root, 1000, 800);
+        //scene.getStylesheets().add(getClass().getResource("/views/css/fondo.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
