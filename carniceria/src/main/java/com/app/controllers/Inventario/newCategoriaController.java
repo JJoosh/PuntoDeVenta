c78a091f57e8bd5b938971cbce32520968f65e37
Arturo Torres <128834206+SystemTrabu@users.noreply.github.com>
2024-06-14 04:56:54 -0600

    ASD

---
+++ carniceria/src/main/java/com/app/controllers/Inventario/newCategoriaController.java
@@ -0,0 +1,13 @@
+package com.app.controllers.Inventario;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class newCategoriaController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        
+    }
+}
